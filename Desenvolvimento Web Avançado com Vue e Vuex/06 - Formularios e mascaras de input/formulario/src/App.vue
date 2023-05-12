553a0d6bf4968f270369dccc94ae7dafb3ae582e
isabellacpmelo <isacpmelo@gmail.com>
2023-05-12 12:09:23 -0300

    feat: Starting project 'Formulario'

---
+++ Desenvolvimento Web Avançado com Vue e Vuex/06 - Formularios e mascaras de input/formulario/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Formulario />
+</template>
+
+<script>
+import Formulario from './components/Formulario.vue'
+export default {
+  name: 'App',
+  components: {
+    Formulario
+  }
+}
+</script>
