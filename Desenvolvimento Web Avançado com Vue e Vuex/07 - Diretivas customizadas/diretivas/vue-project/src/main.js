6a596bdfd173e4b4f685c1edbcb9f80d12dc1c10
isabellacpmelo <isacpmelo@gmail.com>
2024-05-20 19:46:00 -0300

    feat: Section 10 - Modifying display based on argument

--- Desenvolvimento Web Avançado com Vue e Vuex/07 - Diretivas customizadas/diretivas/vue-project/src/main.js
+++ Desenvolvimento Web Avançado com Vue e Vuex/07 - Diretivas customizadas/diretivas/vue-project/src/main.js
@@ -45,21 +45,56 @@ vue.directive('informacao', {
         console.log('el:', el)
         console.log('binding:', binding)
 
-        let funcao = function() {
-            let informacaoSpan = document.createElement('spam')
-            informacaoSpan.style.position = 'absolute'
-            informacaoSpan.style.padding = '2px'
-            informacaoSpan.style.background = '#ddd'
-            informacaoSpan.style.fontSize = '8px'
-            informacaoSpan.style.color = '#000'
-            informacaoSpan.innerText = binding.value
+        let funcao = null
 
-            el.appendChild(informacaoSpan)
+        if (binding.arg === undefined || binding.arg === 'simples') {
+            funcao = function() {
+                let informacaoSpan = document.createElement('spam')
+                informacaoSpan.style.position = 'absolute'
+                informacaoSpan.style.padding = '2px'
+                informacaoSpan.style.background = '#ddd'
+                informacaoSpan.style.fontSize = '8px'
+                informacaoSpan.style.color = '#000'
+                informacaoSpan.innerText = binding.value
 
-            informacaoSpan.addEventListener('click', (event) => {
-                event.stopPropagation()
-                informacaoSpan.remove()
-            })
+                el.appendChild(informacaoSpan)
+
+                informacaoSpan.addEventListener('click', (event) => {
+                    event.stopPropagation()
+                    informacaoSpan.remove()
+                })
+            }
+        }
+
+        if (binding.arg === 'destacado') {
+            funcao = function() {
+                let informacaoDivContainer = document.createElement('div')
+                let informacaoDiv = document.createElement('div')
+                informacaoDiv.innerText = binding.value
+                
+                informacaoDivContainer.style.width = '100%'
+                informacaoDivContainer.style.height = '100%'
+                informacaoDivContainer.style.display = 'flex'
+                informacaoDivContainer.style.flexDirection = 'row'
+                informacaoDivContainer.style.alignItems = 'center'
+                informacaoDivContainer.style.justifyContent = 'center'
+                informacaoDivContainer.style.background = '#ddd'
+                informacaoDivContainer.style.position = 'absolute'
+                informacaoDivContainer.style.top = '0'
+                informacaoDivContainer.style.zIndex= '10'
+
+                informacaoDiv.style.padding = '30px'
+                informacaoDiv.style.background = '#fba'
+                informacaoDiv.style.color = '#000'
+
+                informacaoDivContainer.appendChild(informacaoDiv)
+                el.appendChild(informacaoDivContainer)
+
+                informacaoDivContainer.addEventListener('click', (event) => {
+                    event.stopPropagation()
+                    informacaoDivContainer.remove()
+                })
+            }
         }
 
         if (binding.modifiers['umClickMouse']) {
