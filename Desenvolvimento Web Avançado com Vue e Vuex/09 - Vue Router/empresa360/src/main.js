86dce441a993e6e5d07beefcd17056f6c206c834
isabellacpmelo <isacpmelo@gmail.com>
2024-05-25 20:46:11 -0300

    Section 12 - Configuring the Vue Router in a Vue 3 project

--- Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/main.js
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/main.js
@@ -3,4 +3,29 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//  -- inicio rotas
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+
+const routes = [
+    {
+        path: '/home',
+        component: Home,
+    },
+    {
+        path: '/login',
+        component: Login,
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes, // routes: routes,
+})
+
+// -- fim rodas
+
+const Vue = createApp(App)
+Vue.use(router) // adicionando as configurações de rotas a instância do Vue
+Vue.mount('#app')
---
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/views/Login.vue
@@ -0,0 +1,4 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <h3>Login</h3>
+</template>
