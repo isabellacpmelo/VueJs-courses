de4d24a3eda8a88d099643b2c3f2e82366bca5df
isabellacpmelo <isacpmelo@gmail.com>
2024-05-24 21:33:13 -0300

    feat: Section 11 - Adding records to ListaFilmes

--- Desenvolvimento Web Avançado com Vue e Vuex/08 - Mixins/mixins/src/components/ListaFilmes.vue
+++ Desenvolvimento Web Avançado com Vue e Vuex/08 - Mixins/mixins/src/components/ListaFilmes.vue
@@ -1,9 +1,34 @@
 <script>
 export default {
-    name: 'ListaFilmes'
+    name: 'ListaFilmes',
+    data: () => ({
+        filmes: [],
+        filme: '',
+    }),
+    methods: {
+        adicionarFilme() {
+            this.filmes.push(this.filme)
+        }
+    }
 }
 </script>
 
 <template>
-    <h3>Lista Filmes</h3>
+    <div>
+        <h3>Lista Filmes</h3>
+        <input
+            v-model="filme"
+            type="text"
+            placeholder="Nome do Filme"
+        >
+        <button type="button" @click="adicionarFilme">
+            Adicionar
+        </button>
+        <br>
+        <ul>
+            <li v-for="(filme, index) in filmes" :key="index">
+                {{ filme }}
+            </li>
+        </ul>
+    </div>
 </template>
