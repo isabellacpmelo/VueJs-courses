bf8e0f8aaaf23195e7ddacdc4c2b54eaf50c2f95
isabellacpmelo <isacpmelo@gmail.com>
2024-06-03 21:03:41 -0300

    feat: Section 12 - Router link - Navigating between routes

--- Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/views/Home.vue
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/views/Home.vue
@@ -1,4 +1,5 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <h3>Home</h3>
+    <router-link :to="{ path: '/login' }">Navegar para "/login"</router-link>
 </template>
--- Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/views/Login.vue
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/views/Login.vue
@@ -1,4 +1,5 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <h3>Login</h3>
+    <a href="/home">Navegar para "/home"</a>
 </template>
