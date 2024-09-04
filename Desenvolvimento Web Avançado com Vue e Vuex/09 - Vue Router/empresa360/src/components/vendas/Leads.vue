773b617f88274f7cf40960390d09b16b0c8285ad
isabellacpmelo <isacpmelo@gmail.com>
2024-09-04 20:35:42 -0300

    feat: Section 12 - Leads - HTTP Request for fake API

--- Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/vendas/Leads.vue
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/vendas/Leads.vue
@@ -1,4 +1,26 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <h5>Leads</h5>
+    {{ dados }}
 </template>
+
+<script>
+export default {
+    data: () => ({
+        dados: null
+    }),
+    methods: {
+        getDadosApi() {
+            fetch('http://localhost:3000/leads')
+                .then(response => response.json())
+                .then(response => {
+                    this.dados = response
+                })
+        }
+    },
+    created() {
+        this.getDadosApi()
+    }
+
+}
+</script>
