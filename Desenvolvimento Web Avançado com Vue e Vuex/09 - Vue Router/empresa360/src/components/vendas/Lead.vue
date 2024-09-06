c4e79fae098208aa8cad0279320e76d2b220cda6
isabellacpmelo <isacpmelo@gmail.com>
2024-09-06 20:23:12 -0300

    feat: Section 12 - Route parameters part 2 - Receiving parameters via $route.params

--- Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/vendas/Lead.vue
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/vendas/Lead.vue
@@ -1,4 +1,13 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<script>
+export default {
+    name: 'Lead',
+    // created () {
+    //     console.log(this.$route.params.id)
+    // },
+}
+</script>
+
 <template>
-    <h5>Detalhes do Lead</h5>
+    <h5>Detalhes do Lead {{ $route.params.id }}</h5>
 </template>
