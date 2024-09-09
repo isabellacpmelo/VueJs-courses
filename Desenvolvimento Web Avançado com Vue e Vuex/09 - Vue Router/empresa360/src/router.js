ffd638eff4612aa09ec0bf712ede238fea83875b
isabellacpmelo <isacpmelo@gmail.com>
2024-09-09 16:10:46 -0300

    feat: Section 12 - Standard component in aligned routes (child routes)

--- Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/router.js
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/router.js
@@ -6,6 +6,7 @@ import Site from '@/views/Site.vue'
 import Servicos from '@/components/servicos/Servicos.vue'
 import Dashboard from '@/components/dashboard/Dashboard.vue';
 import Vendas from '@/components/vendas/Vendas.vue'
+import VendasPadrao from '@/components/vendas/VendasPadrao.vue'
 import Leads from '@/components/vendas/Leads.vue'
 import Lead from '@/components/vendas/Lead.vue'
 import Contratos from '@/components/vendas/Contratos.vue'
@@ -34,6 +35,10 @@ const routes = [
                     {
                         path: 'contratos',
                         component: Contratos,
+                    },
+                    {
+                        path: '',
+                        component: VendasPadrao,
                     },
                 ]
             },
