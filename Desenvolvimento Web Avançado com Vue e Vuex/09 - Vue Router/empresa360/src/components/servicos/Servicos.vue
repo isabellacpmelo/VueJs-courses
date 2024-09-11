8ef18c5ff20a511b448e41a313f5937a98f80b2b
isabellacpmelo <isacpmelo@gmail.com>
2024-09-10 21:34:32 -0300

    feat: Section 12 - Services - Listing records

--- Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/servicos/Servicos.vue
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/servicos/Servicos.vue
@@ -1,4 +1,31 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<script>
+import ApiMixin from '@/mixins/ApiMixin'
+export default {
+    name: 'Servicos',
+    mixins: [ApiMixin],
+    created() {
+        this.getDadosApi('http://localhost:3000/servicos')
+    }
+
+}
+</script>
+
 <template>
-    <h5>Serviços</h5>
+    <div>
+        <h5>Serviços</h5>
+        <hr>
+        <div class="row">
+            <div v-for="dado in dados" :key="dado.id" class="col">
+                <div class="card" style="width: 11rem">
+                    <img class="card-img-top" :src="`/img/${dado.icone}`">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                            {{ dado.servico}}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
