bbc0b9b4275db076396019477fad706720a5c51c
isabellacpmelo <isacpmelo@gmail.com>
2024-09-18 20:30:30 -0300

    feat: Section 12 - Reacting to url changes with beforeRouteUpdate

--- Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/servicos/Servico.vue
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/servicos/Servico.vue
@@ -7,12 +7,22 @@ export default {
     created () {
         this.getDadosApi(`http://localhost:3000/servicos/${this.$route.params.id}`)
     },
-    watch: {
-        // eslint-disable-next-line no-unused-vars
-        $route(to, from) {
-            if (to.params.id != undefined)
-                this.getDadosApi(`http://localhost:3000/servicos/${to.params.id}`)
-        }
+    // watch: {
+    //     // eslint-disable-next-line no-unused-vars
+    //     $route(to, from) {
+    //         if (to.params.id != undefined)
+    //             this.getDadosApi(`http://localhost:3000/servicos/${to.params.id}`)
+    //     }
+    // },
+    // eslint-disable-next-line no-unused-vars
+    beforeRouteUpdate(to, from, next) {
+        // to: $route para onde estamos indo
+        // from: $route de onde estamos vindo
+        // next: faz com que o fluxo de navegação siga em frente
+
+        if (to.params.id != undefined)
+            this.getDadosApi(`http://localhost:3000/servicos/${to.params.id}`)
+        next()
     },
 }
 </script>
