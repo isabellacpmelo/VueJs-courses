8ce9c6868824b9914167812bd9c20d3c9f6106b1
isabellacpmelo <isacpmelo@gmail.com>
2024-09-22 22:03:00 -0300

    feat: Section 12 - Contracts - Listing records

--- Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/vendas/Contratos.vue
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/vendas/Contratos.vue
@@ -1,4 +1,38 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<script>
+import ApiMixin from '@/mixins/ApiMixin'
+export default {
+    name: 'Leads',
+    mixins: [ApiMixin],
+    created() {
+        this.getDadosApi('http://localhost:3000/contratos')
+    }
+
+}
+</script>
+
 <template>
-    <h5>Contratos</h5>
+    <div>
+        <h5>Contratos</h5>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Lead</th>
+                    <th scope="col">Serviço</th>
+                    <th scope="col">Data Início</th>
+                    <th scope="col">Data Fim</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="dado in dados" :key="dado.id">
+                    <td>{{ dado.id }}</td>
+                    <td>{{ dado.leadId }}</td>
+                    <td>{{ dado.servicoId }}</td>
+                    <td>{{ dado.data_inicio }}</td>
+                    <td>{{ dado.data_fim }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
