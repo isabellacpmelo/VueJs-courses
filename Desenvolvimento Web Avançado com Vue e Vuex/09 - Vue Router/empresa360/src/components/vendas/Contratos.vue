c807df4b67e33bcb7d3a01d6204c5fc5cdae877c
isabellacpmelo <isacpmelo@gmail.com>
2024-09-23 19:10:02 -0300

    feat: Section 12 - Query Params via router-link

--- Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/vendas/Contratos.vue
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/components/vendas/Contratos.vue
@@ -4,8 +4,21 @@ import ApiMixin from '@/mixins/ApiMixin'
 export default {
     name: 'Leads',
     mixins: [ApiMixin],
+    data: () => ({
+        parametrosDeRelacionamento: '_expand=lead&_expand=servico',
+    }),
     created() {
-        this.getDadosApi('http://localhost:3000/contratos?_expand=lead&_expand=servico')
+        this.getDadosApi(`http://localhost:3000/contratos?${this.parametrosDeRelacionamento}`)
+    },
+    beforeRouteUpdate(to, from, next) {
+        // console.log(to.query) // objeto => URLSearchParams
+        const queryParams = new URLSearchParams(to.query).toString()
+        // console.log(queryParams)
+        const url = `http://localhost:3000/contratos?${this.parametrosDeRelacionamento}&${queryParams}`
+        console.log(url)
+        this.getDadosApi(url)
+
+        next()
     }
 
 }
@@ -14,6 +27,24 @@ export default {
 <template>
     <div>
         <h5>Contratos</h5>
+        teste
+
+        <router-link class="btn btn-primary" :to="{ name: 'contratos', query: {leadId_like: 1 }}">
+            LeadId = 1
+        </router-link>
+
+        <router-link class="btn btn-primary" to="/home/vendas/contratos?servicoId_like=2">
+            ServicoId = 2
+        </router-link>
+
+        <router-link class="btn btn-success" :to="{ name: 'contratos', query: {leadId_like: 1, servicoId_like: 2 }}">
+            LeadId = 1 e ServicoId = 2
+        </router-link>
+
+        <router-link class="btn btn-success" to="/home/vendas/contratos?servicoId_like=2&leadId_like=2">
+            ServicoId = 2 e LeadId = 2
+        </router-link>
+
         <table class="table table-hover">
             <thead>
                 <tr>
