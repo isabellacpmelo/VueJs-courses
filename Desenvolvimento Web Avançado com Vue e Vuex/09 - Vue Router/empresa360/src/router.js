e1371d26f20409e8437c4fcf5ac738b45b509e0e
isabellacpmelo <isacpmelo@gmail.com>
2024-10-07 20:39:37 -0300

    feat: Section 12 - Scroll control part 2 - Implementing the scrollBehavior method

--- Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/router.js
+++ Desenvolvimento Web Avançado com Vue e Vuex/09 - Vue Router/empresa360/src/router.js
@@ -158,6 +158,9 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(),
+    scrollBehavior() {
+        return { left: 150, top: 150 } // left = x; top = y
+    },
     routes, // routes: routes,
 })
 
