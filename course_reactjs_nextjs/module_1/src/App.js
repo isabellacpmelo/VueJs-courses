6076f9406474f7353ddd7caa1318294bbf8dc34b
isabellacpmelo <isacpmelo@gmail.com>
2024-11-20 16:40:34 -0300

    feat: Stateful Class Components

--- course_reactjs_nextjs/module_1/src/App.js
+++ course_reactjs_nextjs/module_1/src/App.js
@@ -3,47 +3,49 @@ import './App.css';
 import { Component } from 'react'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handlePClick = this.handlePClick.bind(this)
+
+    this.state = {
+      name: 'Isabella Melo',
+      counter: 0
+    }
+  }
+
+  handlePClick() {
+    this.setState({ name: 'Isa' })
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault()
+
+    const { counter } = this.state
+    this.setState({ counter: counter + 1 })
+  }
+
   render() {
+    const { name, counter } = this.state
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p onClick={this.handlePClick}>
+            {name} {counter}
           </p>
           <a
+            onClick={this.handleAClick}
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Este é o link
           </a>
         </header>
       </div>
     );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
