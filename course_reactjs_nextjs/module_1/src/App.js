106b1be26e3d09499d007de45a9240011a7a0c4f
isabellacpmelo <isacpmelo@gmail.com>
2024-11-20 18:44:58 -0300

    feat: State with arrays and objects

--- course_reactjs_nextjs/module_1/src/App.js
+++ course_reactjs_nextjs/module_1/src/App.js
@@ -1,48 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handlePClick = this.handlePClick.bind(this)
-
-    this.state = {
-      name: 'Isabella Melo',
-      counter: 0
-    }
-  }
-
-  handlePClick() {
-    this.setState({ name: 'Isa' })
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault()
-
-    const { counter } = this.state
-    this.setState({ counter: counter + 1 })
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'O titulo 1',
+        body: 'O corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O titulo 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O titulo 3',
+        body: 'O corpo 3'
+      },
+    ]
   }
 
   render() {
-    const { name, counter } = this.state
+    const { posts } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Este é o link
-          </a>
-        </header>
+        { posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
