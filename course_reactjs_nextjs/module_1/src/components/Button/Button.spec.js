d91ca154b79d9135fdfa65661f93fa50482cbace
isabellacpmelo <isacpmelo@gmail.com>
2024-12-05 21:07:23 -0300

    feat: Introduction to testing with Jest and Testing Library - Part 2

--- course_reactjs_nextjs/module_1/src/components/Button/Button.spec.js
+++ course_reactjs_nextjs/module_1/src/components/Button/Button.spec.js
@@ -1,11 +1,48 @@
 import { render, screen } from '@testing-library/react'
+// import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { Button } from '.'
 
 describe('<Button  />', () => {
-it('should render the button with the text "Load more"', () => {
+    test('should render the button with the text "Load more"', () => {
         render(<Button text="Load more"/>)
+        expect.assertions(1)
 
         const button = screen.getByRole('button', { name: /load more/i })
+        // expect(button).toHaveAttribute('class', 'button')
         expect(button).toBeInTheDocument()
     })
+
+    test('should call function on button click', async () => {
+        const fn = jest.fn()
+        render(<Button text="Load more" onClick={fn} />)
+
+        const button = screen.getByRole('button', { name: /load more/i })
+
+        await userEvent.click(button)
+        // fireEvent.click(button)
+
+        expect(fn).toHaveBeenCalledTimes(1)
+
+    })
+
+    test('should be disabled when disabled is true', () => {
+        render(<Button text="Load more" disabled={true} />)
+
+        const button = screen.getByRole('button', { name: /load more/i })
+
+        expect(button).toBeDisabled()
+    })
+
+    test('should be not enabled when disabled is false', () => {
+        render(<Button text="Load more" disabled={false} />)
+
+        const button = screen.getByRole('button', { name: /load more/i })
+
+        expect(button).not.toBeDisabled()
+    })
 })
+
+// Me diga por que o npm test -- --coverage não está funcionando?
+
+// Me diga por que o npm test -- --coverage --watchAll nao funciona?
--- course_reactjs_nextjs/module_1/src/index.js
+++ course_reactjs_nextjs/module_1/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+ 
 import './styles/global-styles.css';
+ 
 import { Home } from './templates/Home';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
+ 
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Home tab="home" />);
 
 
