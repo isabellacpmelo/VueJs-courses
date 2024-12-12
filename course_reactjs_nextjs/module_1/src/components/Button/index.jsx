11e321457915a22ec26f4268d32174158dff8f17
isabellacpmelo <isacpmelo@gmail.com>
2024-12-12 17:06:09 -0300

    feat: Eslint, prettier, editorconfig and prop-types - Part 2

--- course_reactjs_nextjs/module_1/src/components/Button/index.jsx
+++ course_reactjs_nextjs/module_1/src/components/Button/index.jsx
@@ -1,18 +1,23 @@
+import P from 'prop-types'
 import './styles.css'
 
-import { Component } from "react";
+export const Button = ({ text, onClick, disabled }) => (
+  <button
+      disabled ={disabled}
+      className='button'
+      onClick={ onClick }
+  >
+      {text}
+  </button>
+)
 
-export class Button extends Component {
-    render() {
-        const { text, onClick, disabled } = this.props
-        return (
-            <button
-                disabled ={disabled}
-                className='button'
-                onClick={ onClick }
-            >
-                {text}
-            </button>
-        )
-    }
+button.defaltProps = {
+  disabled: false,
+  
+}
+
+button.propTypes = {
+  text: P.string.isRequired,
+  onClick: P.func.isRequired,
+  disabled: P.bool
 }
