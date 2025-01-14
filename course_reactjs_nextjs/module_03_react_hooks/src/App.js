3568a9a3aae9ed3cc4599e5770bea500cece5de4
isabellacpmelo <isacpmelo@gmail.com>
2025-01-14 18:39:32 -0300

    feat: useMemo - React Hook (part 3)

--- course_reactjs_nextjs/module_03_react_hooks/src/App.js
+++ course_reactjs_nextjs/module_03_react_hooks/src/App.js
@@ -1,31 +1,57 @@
 /* eslint-disable no-unused-vars */
 import P from 'prop-types'
 import './App.css'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
+
+const Post = ({ post }) => {
+  console.log('Filho renderizou')
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  )
+}
+
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
+}
 
 function App() {
   const [posts, setPosts] = useState([])
+  const [value, setValue] = useState('')
   console.log('Pai renderizou')
 
   // Component did mount
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((result) => result.json())
-      .then((result) => setPosts(result))
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((result) => result.json())
+        .then((result) => setPosts(result))
+    }, 5000)
   }, [])
-
-  console.log(posts)
 
   return (
     <div className="App">
-      {posts.map((post) => {
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {useMemo(() => {
         return (
-          <div key={post.id} className="post">
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-          </div>
+          posts.length > 0 &&
+          posts.map((post) => <Post key={post.id} post={post} />)
         )
-      })}
+      }, [posts])}
+
+      {posts.length <= 0 && <p>Ainda não existem posts</p>}
     </div>
   )
 }
