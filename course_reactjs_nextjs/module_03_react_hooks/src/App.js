4a20f3f8bc1d1a244b1122d235bf73d9417917ea
isabellacpmelo <isacpmelo@gmail.com>
2025-01-17 19:12:18 -0300

    feat: useContext - React Hook - Part 2

--- course_reactjs_nextjs/module_03_react_hooks/src/App.js
+++ course_reactjs_nextjs/module_03_react_hooks/src/App.js
@@ -1,58 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React, { useContext, useState } from 'react'
 import './App.css'
-
-const globalState = {
-  title: 'O título do contexto',
-  body: 'O body do contexto',
-  counter: 0,
-}
-
-const GlobalContext = React.createContext()
-
-const Div = ({ children }) => {
-  return (
-    <div>
-      <H1 />
-      <P />
-    </div>
-  )
-}
-
-const H1 = () => {
-  const theContext = useContext(GlobalContext)
-  const {
-    contextState: { title, counter },
-  } = theContext
-  return (
-    <h1>
-      {title} {counter}
-    </h1>
-  )
-}
-
-const P = () => {
-  const theContext = useContext(GlobalContext)
-  const {
-    contextState: { body, counter },
-    contextState,
-    setContextState,
-  } = theContext
-  return (
-    <p
-      onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}
-    >
-      {body}
-    </p>
-  )
-}
+import { AppContext } from './contexts/AppContext'
+import { Div } from './components/Div'
 
 function App() {
-  const [contextState, setContextState] = useState(globalState)
   return (
-    <GlobalContext.Provider value={{ contextState, setContextState }}>
+    <AppContext>
       <Div />
-    </GlobalContext.Provider>
+    </AppContext>
   )
 }
 
---
+++ course_reactjs_nextjs/module_03_react_hooks/src/components/Div/index.jsx
@@ -0,0 +1,11 @@
+import { H1 } from '../H1'
+import { P } from '../P'
+
+export const Div = ({ children }) => {
+  return (
+    <div>
+      <H1 />
+      <P />
+    </div>
+  )
+}
