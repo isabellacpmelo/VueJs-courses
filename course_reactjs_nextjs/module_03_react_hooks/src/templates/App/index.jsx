8b2160c69ffd74816f7d8ad6bc758829a2af5694
isabellacpmelo <isacpmelo@gmail.com>
2025-01-25 17:45:20 -0300

    feat: Creating my second hook - Part 1

--- course_reactjs_nextjs/module_03_react_hooks/src/templates/App/index.jsx
+++ course_reactjs_nextjs/module_03_react_hooks/src/templates/App/index.jsx
@@ -1,18 +1,51 @@
-import { Posts } from '../../components/Posts'
-import { PostsProvider } from '../../contexts/PostsProvider'
-import { CounterProvider } from '../../contexts/ExampleProvider'
+import { useEffect, useState } from 'react'
 import './style.css'
 
+const useFetch = (url, options) => {
+  const [result, setResult] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    console.log('EFFECT', new Date().toLocaleString())
+    setLoading(true)
+
+    const fetchData = async () => {
+      await new Promise((r) => setTimeout(r, 3000))
+      try {
+        const response = await fetch(url, options)
+        const jsonResult = await response.json()
+        setResult(jsonResult)
+        setLoading(false)
+      } catch (error) {
+        setLoading(false)
+        throw error
+      }
+    }
+
+    fetchData()
+  }, [url, options])
+
+  return [result, loading]
+}
+
 function App() {
-  return (
-    <CounterProvider>
-      <PostsProvider>
-        <div>
-          <Posts />
-        </div>
-      </PostsProvider>
-    </CounterProvider>
+  const [result, loading] = useFetch(
+    'https://jsonplaceholder.typicode.com/posts',
+    {
+      method: 'GET',
+      Headers: {
+        abc: '1',
+      },
+    },
   )
+
+  if (loading) return <p>Loading...</p>
+
+  if (!loading && result) {
+    console.log(result)
+  }
+
+  return <h1>Oi</h1>
 }
 
 export default App
