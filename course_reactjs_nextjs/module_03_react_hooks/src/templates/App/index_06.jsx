2b5320ff45bfb39dac2021633ab03702e12e32b2
isabellacpmelo <isacpmelo@gmail.com>
2025-02-02 18:53:53 -0300

    feat: Compound Components Pattern and React.Children + React.cloneElement - Part 1

---
+++ course_reactjs_nextjs/module_03_react_hooks/src/templates/App/index_06.jsx
@@ -0,0 +1,90 @@
+import { Component, useEffect, useState } from 'react'
+
+const s = {
+  style: {
+    fontSize: '60px',
+  },
+}
+
+class MyErrorBoundary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(error) {
+    // Atualiza o state para que a próxima renderização mostre a UI alternativa.
+    return { hasError: true }
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Você também pode registrar o erro em um serviço de relatórios de erro
+    // console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Você pode renderizar qualquer UI alternativa
+      return <p {...s}>Deu ruim =(</p>
+    }
+
+    return this.props.children
+  }
+}
+
+const ItWillThrowError = () => {
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    if (counter > 3) {
+      throw new Error('Que chato!!!')
+    }
+  }, [counter])
+
+  return (
+    <div>
+      <button {...s} onClick={() => setCounter((s) => s + 1)}>
+        Click to increase {counter}
+      </button>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div {...s}>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+      <MyErrorBoundary>
+        <ItWillThrowError />
+      </MyErrorBoundary>
+    </div>
+  )
+}
+
+export default App
