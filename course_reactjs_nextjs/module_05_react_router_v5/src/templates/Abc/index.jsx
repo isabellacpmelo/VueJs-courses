9e46e32bb7083abfbc52278cc43f77e687bf3bee
isabellacpmelo <isacpmelo@gmail.com>
2025-02-09 15:18:54 -0300

    feat: useHistory and useLocation

--- course_reactjs_nextjs/module_05_react_router_v5/src/templates/Abc/index.jsx
+++ course_reactjs_nextjs/module_05_react_router_v5/src/templates/Abc/index.jsx
@@ -1,7 +1,18 @@
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useParams, useLocation, useHistory } from 'react-router-dom';
 
 export const Abc = () => {
   const { slug, id } = useParams();
+  const location = useLocation();
+  const history = useHistory();
+
+  console.log(location);
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.push('/');
+    }, 5000);
+  }, [history]);
 
   return (
     <div>
--- course_reactjs_nextjs/module_05_react_router_v5/src/templates/App/index.jsx
+++ course_reactjs_nextjs/module_05_react_router_v5/src/templates/App/index.jsx
@@ -1,10 +1,19 @@
 import './styles.css';
-
 import { PostsProvider } from '../../contexts/PostsProvider';
 import { Posts } from '../../components/Posts';
 import { CounterProvider } from '../../contexts/CounterProvider';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export function App() {
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.push('/abc/slug/id');
+    }, 5000);
+  }, [history]);
+
   return (
     <CounterProvider>
       <PostsProvider>
