20856ca6f096f12ff59e62edae15d3cfda8cf9fd
isabellacpmelo <isacpmelo@gmail.com>
2025-02-10 18:44:03 -0300

    feat: Installing React Router Dom v6 and configuring the project

---
+++ course_reactjs_nextjs/module_06_react_router_v6/src/components/Home/index.tsx
@@ -0,0 +1,10 @@
+/** @format */
+import "./style.css";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
--- course_reactjs_nextjs/module_06_react_router_v6/src/main.tsx
+++ course_reactjs_nextjs/module_06_react_router_v6/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+/** @format */
 
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/global.css";
+import { Home } from "./components/Home";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Home />
+  </StrictMode>
+);
